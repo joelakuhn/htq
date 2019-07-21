c2a404878f1d17e25e08e5d92c7557975aae0f3d
Joel <joelakuhn@gmail.com>
2019-07-21 09:19:10 -0400

    CSS queries are working using the example code from libmodest.

--- main.c
+++ main.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <getopt.h>
 
-#include <myhtml/myhtml.h>
-#include <myhtml/serialization.h>
+#include "css_query.c"
 
 #include "str_arr.c"
 
@@ -54,6 +53,59 @@ int main(int argc, char **argv) {
         str_arr_push(&options.css_queries, str_arr_shift(&files));
     }
 
+    char* selector = options.css_queries.strs[0];
+
+    struct res_html res = load_html_file(files.strs[0]);
+
+    // const char *selector = "div > :nth-child(2n+1):not(:has(a))";
+
+    /* init MyHTML and parse HTML */
+    myhtml_tree_t *html_tree = parse_html(res.html, strlen(res.html));
+
+    /* create css parser and finder for selectors */
+    mycss_entry_t *css_entry = create_css_parser();
+    modest_finder_t *finder = modest_finder_create_simple();
+
+    /* parse selectors */
+    mycss_selectors_list_t *selectors_list = prepare_selector(css_entry, selector, strlen(selector));
+
+    /* find nodes by selector */
+    myhtml_collection_t *collection = NULL;
+    modest_finder_by_selectors_list(finder, html_tree->node_html, selectors_list, &collection);
+
+    /* print result */
+    /* print selector */
+    printf("Incoming selector:\n\t");
+    mycss_selectors_serialization_list(mycss_entry_selectors(css_entry),
+                                       selectors_list, serialization_callback, NULL);
+    printf("\n\n");
+
+    /* print tree */
+    // printf("Incoming tree:\n\t");
+    // myhtml_serialization_tree_callback(html_tree->node_html, serialization_callback, NULL);
+
+    /* print found result */
+    printf("\n\nFound nodes:");
+    print_found_result(html_tree, collection);
+
+    /* destroy all */
+    myhtml_collection_destroy(collection);
+
+    /* destroy selector list */
+    mycss_selectors_list_destroy(mycss_entry_selectors(css_entry), selectors_list, true);
+
+    /* destroy Modest Finder */
+    modest_finder_destroy(finder, true);
+
+    /* destroy MyCSS */
+    mycss_t *mycss = css_entry->mycss;
+    mycss_entry_destroy(css_entry, true);
+    mycss_destroy(mycss, true);
+
+    /* destroy MyHTML */
+    myhtml_t* myhtml = html_tree->myhtml;
+    myhtml_tree_destroy(html_tree);
+    myhtml_destroy(myhtml);
 
 
 }
